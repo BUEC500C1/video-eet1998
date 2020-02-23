a950477b4e408042b0f482aa36b9ea5c7de8b2aa
Erin Thomas <erin.thomas@Alan-Turing.local>
2020-02-23 12:21:07 -0500

    Initialized consumer keys and access tokens for API

--- test_summarizer.py
+++ test_summarizer.py
@@ -2,6 +2,11 @@ import os
 import tweepy as tw
 import pandas as pd
 
+consumer_key= 'yourkeyhere'
+consumer_secret= 'yourkeyhere'
+access_token= 'yourkeyhere'
+access_token_secret= 'yourkeyhere'
+
 auth = tw.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
-api = tw.API(auth, wait_on_rate_limit=True)
+api = tw.API(auth)
