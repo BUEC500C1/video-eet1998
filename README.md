8389269d63345037e141e4d2a777b263e1c61fab
Erin Thomas <erin.thomas@dhcp-wifi-8021x-155-41-90-239.bu.edu>
2020-02-26 16:20:54 -0500

    Simplified get_tweets()

--- README.md
+++ README.md
@@ -8,4 +8,5 @@ The purpose of this program is to construct a video for a Twitter handle's tweet
 ## How to Run This Code
 
 ## References
-https://towardsdatascience.com/extracting-twitter-data-pre-processing-and-sentiment-analysis-using-python-3-0-7192bd8b47cf
+- https://towardsdatascience.com/extracting-twitter-data-pre-processing-and-sentiment-analysis-using-python-3-0-7192bd8b47cf
+- https://www.geeksforgeeks.org/extraction-of-tweets-using-tweepy/
--- twitter_summarizer.py
+++ twitter_summarizer.py
@@ -1,41 +1,30 @@
 import os
-import tweepy as tw
+import tweepy
 import pandas as pd
 import csv
+import threading
 
 # consumer keys and access tokens
-consumer_key= 'yourkeyhere'
-consumer_secret= 'yourkeyhere'
-access_token= 'yourkeyhere'
-access_token_secret= 'yourkeyhere'
+consumer_key = 'yourkeyhere'
+consumer_secret = 'yourkeyhere'
+access_token = 'yourkeyhere'
+access_token_secret = 'yourkeyhere'
 
 # Twitter API authorization and initilization
-auth = tw.OAuthHandler(consumer_key, consumer_secret)
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
-api = tw.API(auth)
+api = tweepy.API(auth)
 
-def extract_tweets():
-    tweets_list = []
-    new_tweets = api.user_timeline(screen_name=screen_name, count=200)
-    tweets_list.extend(new_tweets)
-
-    # id is updated to oldest tweet - 1 to keep track
-    oldest_tweet = tweets_list[-1].id - 1
-    print('...%s tweets have been downloaded so far' % len(tweets_list))
-
-    # transforming the tweets into a 2D array that will be used to populate the csv
-
-    outtweets = [[tweet.id_str, tweet.created_at,
-    tweet.text.encode('utf-8')] for tweet in tweets_list]
-
-# writing to the csv file
-
-    with open(screen_name + '_tweets.csv', 'w', encoding='utf8') as f:
-        writer = csv.writer(f)
-        writer.writerow(['id', 'created_at', 'text'])
-        writer.writerows(outtweets)
+def get_tweets(handle):
+    #num_tweets = 10
+    tweets = api.user_timeline(screen_name=handle)
+    array = []
+    tweets_for_csv = [tweet.text for tweet in tweets]
+    for i in tweets_for_csv:
+        array.append(i)
+    print(array)
 
 if __name__ == '__main__':
+    get_tweets("twitter-handle")
 
-    tweets_list(input("Enter the twitter handle of the person whose tweets you want to download: "))
 
