e5606970f2cfb1b7d661a9bf5a307f82e5ac0725
Erin Thomas <erin.thomas@Alan-Turing.local>
2020-02-28 18:35:05 -0500

    Created create_directories()

--- twitter_summarizer.py
+++ twitter_summarizer.py
@@ -17,6 +17,21 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
+def create_directories():
+    image_dir_name = 'ImagesDirectory'
+    video_dir_name = 'VideoDirectory'
+    if not os.path.exists(image_dir_name):
+        os.mkdir(image_dir_name)
+        print("Images directory created.")
+    else:
+        print("Images directory already exists.")
+
+    if not os.path.exists(video_dir_name):
+        os.mkdir(video_dir_name)
+        print("Video directory created.")
+    else:
+        print("Video directory already exists.")
+
 def get_tweets(handle):
     #num_tweets = 10
     tweets = api.user_timeline(screen_name=handle)
@@ -35,9 +50,11 @@ def tweet2image(array):
         image = Image.new(mode="RGB", size=(200,70), color="red")
         draw = ImageDraw.Draw(image)
         draw.text((10,10), str(i), font=fnt, fill=(255,255,0))
-        image.save(file_name)
-        os.system(file_name)
+        image.save(path + file_name)
         file_num += 1
+
+def image2video():
+
 
 
 
