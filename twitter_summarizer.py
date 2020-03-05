c29bbac83d6b929d128607a8477f97f059aa91d6
Erin Thomas <erin.thomas@dhcp-wifi-8021x-155-41-3-43.bu.edu>
2020-03-05 13:59:32 -0500

    Update README.md

--- twitter_summarizer.py
+++ twitter_summarizer.py
@@ -35,8 +35,6 @@ class VideoWorker(Thread):
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth)
 
-
-
     def __init__(self, path, queue):
         Thread.__init__(self)
         self.queue = queue
@@ -49,27 +47,7 @@ class VideoWorker(Thread):
         finally:
             # Do something
 
-
-
-    
-
-def main():
-    ts = time()
-    # Do stuff
-    # Create a queue to communicate with worker threads
-    queue = Queue()
-    # Create 4 worker threads
-    for x in range(4):
-        worker = VideoWorker(queue)
-        # Setting daemon to True will let the main 
-        # thread exit even though the workers are blocking
-        worker.daemon = True
-        worker.start()
-
-
-
-
-def get_tweets(handle):
+    def get_tweets(self, handle):
     #num_tweets = 10
     tweets = api.user_timeline(screen_name=handle)
     array = []
@@ -78,7 +56,7 @@ def get_tweets(handle):
         array.append(i)
     return array
 
-def tweet2image(array):
+    def tweet2image(array):
     file_num = 1
     file_name = ""
     path = os.getcwd()
@@ -100,13 +78,23 @@ def tweet2image(array):
             "ffmpeg -r 1/3 -f image2 -s 174x300 -i " +
             "_tweet%d.png -vcodec libx264 -crf 25  -pix_fmt yuv420p " +
             "_twitter_video.mp4")
-'''
+''' 
 
-
+def main():
+    ts = time()
+    # Do stuff
+    # Create a queue to communicate with worker threads
+    queue = Queue()
+    # Create 4 worker threads
+    for x in range(4):
+        worker = VideoWorker(queue)
+        # Setting daemon to True will let the main 
+        # thread exit even though the workers are blocking
+        worker.daemon = True
+        worker.start()
 
 if __name__ == '__main__':
-    array = get_tweets("NatGeo")
-    #tweet2image(array)
+    main()
 
 
 
